916f4ee7388be4795124679908a2aa434c2fac90
Kavit <kavitmht@gmail.com>
2022-02-18 21:12:16 -0800

    made changes to unit test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,64 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { assert } = require('chai')
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('knft Contract', async () => {
+	let nft
+	let nftContractAddress
+	let tokenId
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+	// Deploys the EternalNFT contract and the EternalMarket contract before each test
+	beforeEach('Setup Contract', async () => {
+		const knft = await ethers.getContractFactory('knft')
+		nft = await knft.deploy()
+		await nft.deployed()
+		nftContractAddress = await nft.address
+	})
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+	// Tests address for the EternalNFT contract
+	it('Should have an address', async () => {
+		assert.notEqual(nftContractAddress, 0x0)
+		assert.notEqual(nftContractAddress, '')
+		assert.notEqual(nftContractAddress, null)
+		assert.notEqual(nftContractAddress, undefined)
+	})
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+	// Tests name for the token of EternalNFT contract
+	it('Should have a name', async () => {
+		// Returns the name of the token
+		const name = await nft.collectionName()
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+		assert.equal(name, 'knft')
+	})
+
+	// Tests symbol for the token of EternalNFT contract
+	it('Should have a symbol', async () => {
+		// Returns the symbol of the token
+		const symbol = await nft.collectionSymbol()
+
+		assert.equal(symbol, 'knft')
+	})
+
+	// Tests for NFT minting function of EternalNFT contract using tokenID of the minted NFT
+	it('Should be able to mint NFT', async () => {
+		// Mints a NFT
+		let txn = await nft.createknft()
+		let tx = await txn.wait()
+
+		// tokenID of the minted NFT
+		let event = tx.events[0]
+		let value = event.args[2]
+		tokenId = value.toNumber()
+
+		assert.equal(tokenId, 0)
+
+		// Mints another NFT
+		txn = await nft.createknft()
+		tx = await txn.wait()
+
+		// tokenID of the minted NFT
+		event = tx.events[0]
+		value = event.args[2]
+		tokenId = value.toNumber()
+
+		assert.equal(tokenId, 1)
+	})
+})
